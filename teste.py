2392ac9a595a9e51f707cdda46aafb6f9c1a4fb0
Nobreex <diogo.nobre.23012006@gmail.com>
2025-02-18 19:18:53 -0300

    commit

--- teste.py
+++ teste.py
@@ -1,15 +1,15 @@
 import random
 
-# Gera um número secreto entre 1 e 5
+
 numero_secreto = random.randint(1, 5)
 
 print("Olá! Vamos jogar um jogo! 🎮")
 print("Tente adivinhar o número entre 1 e 5.")
 
-# Pede para o jogador digitar um número
+
 tentativa = int(input("Qual número você acha que é? "))
 
-# Verifica se o jogador acertou
+
 if tentativa == numero_secreto:
     print("Parabéns! 🎉 Você acertou!")
 else:
